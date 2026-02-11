0221bf9e5dd749887d80ceffb42813b7ab9680d8
Bik-dot <83629428+Bik-dot@users.noreply.github.com>
2026-02-11 14:25:48 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -149,7 +149,7 @@ def check_market():
 
             change = ((last - prev) / prev) * 100
 
-            if abs(change) < 0.4:
+            if abs(change) < 0.15:
                 continue
 
             trend = get_trend(data)
